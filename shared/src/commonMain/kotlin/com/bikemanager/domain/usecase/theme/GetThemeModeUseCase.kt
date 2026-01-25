1526934492251e7c150d8151a22bcdb70a15e262
Adrien BOISSIER <33319289+trixidis@users.noreply.github.com>
2026-01-25 14:05:07 +0100

    auto-claude: subtask-3-3 - Create GetThemeModeUseCase

--- shared/src/commonMain/kotlin/com/bikemanager/domain/usecase/theme/GetThemeModeUseCase.kt
+++ shared/src/commonMain/kotlin/com/bikemanager/domain/usecase/theme/GetThemeModeUseCase.kt
@@ -5,7 +5,7 @@ import com.bikemanager.domain.repository.ThemeRepository
 import kotlinx.coroutines.flow.Flow
 
 /**
- * Use case for getting the current theme mode preference.
+ * Use case for getting the current theme mode.
  */
 class GetThemeModeUseCase(private val repository: ThemeRepository) {
     /**
