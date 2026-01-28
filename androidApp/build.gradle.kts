dfbe980d7a1258246b7f58a560d259f992eadb2d
Adrien BOISSIER <33319289+trixidis@users.noreply.github.com>
2026-01-28 09:22:47 +0100

    chore: bump version to 1.4.0 (versionCode 7)
    Prepare release v1.4.0 with incremented version codes for publication to Play Store.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "com.bikemanager.android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.3.0"
+        versionCode = 7
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
