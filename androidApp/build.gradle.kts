1415f97a5e3d1d381edbd2f8c07e0c7b22993f4d
Adrien BOISSIER <33319289+trixidis@users.noreply.github.com>
2026-02-09 23:44:53 +0100

    add full debug symbols for native libraries in android build

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -77,6 +77,7 @@ android {
 
             isMinifyEnabled = true
             isShrinkResources = true
+            ndk { debugSymbolLevel = "FULL" }
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
