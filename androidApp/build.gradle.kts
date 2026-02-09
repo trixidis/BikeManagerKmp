caab9fac09f8cff6e7e8f4cf27106507cab2fbe5
Adrien BOISSIER <33319289+trixidis@users.noreply.github.com>
2026-02-09 18:52:41 +0100

    chore: bump version to 1.4.2

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "com.bikemanager.android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.4.1"
+        versionCode = 8
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
